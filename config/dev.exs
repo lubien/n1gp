a670bf39ebc0993440e842f117b855eb59cb616c
lubien <lubien1996@gmail.com>
2022-10-23 07:47:22 -0300

    Add tailwind

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :n1gp, N1gpWeb.Endpoint,
   secret_key_base: "4iSV9O4xDJRJO6afuqcSspOYrhV4dtOKYyPD8bMqCfZsEE31uN3SN4u8ejbiOFk0",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
